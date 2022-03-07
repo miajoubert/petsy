8091ce12372fd5c6a5f53103b4933bc79ae5d188
Chiew <chiews89@gmail.com>
2022-03-07 15:13:14 -0800

    finished delete and add for products

--- app/forms/__init__.py
+++ app/forms/__init__.py
@@ -1,3 +1,4 @@
 from .login_form import LoginForm
 from .signup_form import SignUpForm
 from .search_form import SearchForm
+from .product_form import ProductForm
---
+++ app/forms/product_form.py
@@ -0,0 +1,13 @@
+from typing import Text
+from flask_wtf import FlaskForm
+from wtforms import StringField, TextAreaField, IntegerField, SubmitField, SelectField
+from wtforms.validators import DataRequired
+
+class ProductForm(FlaskForm):
+    name = StringField('Name', validators=[DataRequired()], message='Name cannot be blank')
+    image = StringField('Image', validators=[DataRequired()], message='Image cannot be blank')
+    description = TextAreaField('Description', validators=[DataRequired()], message='Description cannot be blank')
+    price = IntegerField('Price', validators=[DataRequired()], message='Price cannot be blank')
+    category = SelectField('Category', choices=[('Dog Food', 'Dog Food'), ('Dog Toys', 'Dog Toys'), ('Cat Food', 'Cat Food'), ('Cat Toys', 'Cat Toys'), ('Cat Litter', 'Cat Litter'), ('Bowls and Feeders', 'Bowls and Feeders'), ('Grooming', 'Grooming'), ('Clothing and Accessories', 'Clothing and Accessories')], validators=[DataRequired()], message='Must select a category')
+    categoryId = IntegerField('Category Id', validators=[DataRequired])
+    submit = SubmitField('Submit')
