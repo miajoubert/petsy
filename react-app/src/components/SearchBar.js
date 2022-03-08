8af58741d5ed92bf8457dbc48e9f51968a38e212
Laura <laura.maser3@yahoo.com>
2022-03-07 20:05:00 -0500

    Worked on search state.

--- react-app/src/components/SearchBar.js
+++ react-app/src/components/SearchBar.js
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { listResults } from '../store/results';
 
 const SearchBar = () => {
+    const [ searchTerm, setSearchTerm ] = useState('');
+    const dispatch = useDispatch();
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        setSearchTerm(e.target.value)
+
+        const results = {
+            results: searchTerm
+        }
+
+        const allResults = await dispatch(listResults(results))
+        return allResults;
+    }
+
     return (
         <form action='/results'>
-            <input type='search' placeholder='Search Products' name='search' required />
+            <input type='search' placeholder='Search Products' name='search' required value={searchTerm} onChange={handleSubmit}/>
             <button>Search</button>
         </form>
     )
