c68db61ed09d863702cfeeea9a30b3b51cd9388c
Chiew <chiews89@gmail.com>
2022-03-07 17:00:02 -0800

    added productspages

--- react-app/src/store/index.js
+++ react-app/src/store/index.js
@@ -2,10 +2,12 @@ import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import session from './session'
 import resultsReducer from './results'
+import productsReducer from './products';
 
 const rootReducer = combineReducers({
   session,
-  resultsReducer
+  resultsReducer,
+  productsReducer,
 });
 
 
