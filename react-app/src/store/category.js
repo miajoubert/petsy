7971b6298c7cf6d67c885f22b2acdce1231fc904
Laura <laura.maser3@yahoo.com>
2022-03-10 22:50:18 -0500

    Category detail pages component complete.

--- react-app/src/store/category.js
+++ react-app/src/store/category.js
@@ -1,17 +1,9 @@
 const LOAD_CATEGORIES = "categories/LOAD_CATEGORIES";
-const LOAD_CATEGORY = "categories/LOAD_CATEGORY";
 
 const loadCategories = (categories) => {
   return {
     type: LOAD_CATEGORIES,
     categories,
-  };
-};
-
-const loadCategory = (category) => {
-  return {
-    type: LOAD_CATEGORY,
-    category,
   };
 };
 
@@ -26,17 +18,6 @@ export const getCategories = () => async (dispatch) => {
   return res;
 };
 
-export const getCategory = (id) => async (dispatch) => {
-  const res = await fetch(`api/categories/${id}`);
-
-  if (res.ok) {
-    const category = await res.json();
-    dispatch(loadCategory(category));
-  }
-
-  return res;
-};
-
 const categoriesReducer = (state = {}, action) => {
   let newState;
   switch (action.type) {
@@ -46,15 +27,6 @@ const categoriesReducer = (state = {}, action) => {
         newState[category.id] = category;
       });
       return newState;
-    // case LOAD_CATEGORY:
-    // newState = {
-    //     ...state,
-    //     [action.event.id]: {
-    //         ...state[action.event.id],
-    //         ...action.event
-    //     }
-    // };
-    // return newState;
     default:
       return state;
   }
