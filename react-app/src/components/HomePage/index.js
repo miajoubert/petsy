c31f4eb14fccbfca1acf1cc08eeab149ed76bf7d
Laura <laura.maser3@yahoo.com>
2022-03-11 19:40:04 -0500

    Styled home page.

--- react-app/src/components/HomePage/index.js
+++ react-app/src/components/HomePage/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 
@@ -18,39 +18,46 @@ const HomePage = () => {
   }, [dispatch]);
 
   return (
-    <div className='categories-container'>
-      {categories.map((category) => (
-        <div key={category.id} className="category-container">
-          <h2>{category.name}</h2>
-          {category.products.map((product) => (
-            <div key={product?.id}>
-              {product.name}
-              <div>
+    <main>
+      <div className="home-banner">
+        <h1>Explore items for all of your pet's needs</h1>
+      </div>
+      <div className='categories-container'>
+        {categories?.map((category) => (
+          <div key={category?.id} className="category-container">
+            <h2>
+              <Link to={`/categories/${category?.id}`}>
+                {category?.name}
+              </Link>
+            </h2>
+            <div className="home-category-products">
+              {category?.products?.map((product) => (
                 <Link
                   key={`single_product_link_${product?.id}`}
                   to={`/products/${product?.id}`}
                 >
-                  <img
-                    width={300}
-                    height={300}
-                    alt={product?.name}
-                    src={
-                      product?.image_url
-                        ? product?.image_url
-                        : "https://media.istockphoto.com/vectors/no-image-available-sign-vector-id922962354?k=20&m=922962354&s=612x612&w=0&h=f-9tPXlFXtz9vg_-WonCXKCdBuPUevOBkp3DQ-i0xqo="
-                    }
-                  />
+                  <div key={product?.id} className="product-card">
+                    <div>
+                      <img
+                        className="home-product-image"
+                        alt={product?.name}
+                        src={
+                          product?.image_url
+                            ? product?.image_url
+                            : "https://media.istockphoto.com/vectors/no-image-available-sign-vector-id922962354?k=20&m=922962354&s=612x612&w=0&h=f-9tPXlFXtz9vg_-WonCXKCdBuPUevOBkp3DQ-i0xqo="
+                        }
+                      />
+                      <h3 className="home-product-name">{product?.name}</h3>
+                      <h4 className="home-product-price">${parseFloat(product?.price).toFixed(2)}</h4>
+                    </div>
+                  </div>
                 </Link>
-                <h4 className="product_name">{product?.name}{category.name}</h4>
-                <h5 className="product_price">$ {parseFloat(product?.price).toFixed(2)}</h5>
-              </div>
-              <div>{product.price}</div>
+              ))}
             </div>
-          ))}
-          <div></div>
-        </div>
-      ))}
-    </div>
+          </div>
+        ))}
+      </div>
+    </main>
   );
 };
 
