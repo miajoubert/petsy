8bf65a48e96bf1c3c2e9e066853d927ee6d7e569
Laura <laura.maser3@yahoo.com>
2022-03-12 22:52:01 -0500

    Fixed float price on category detail pages and minor styling changes.

--- react-app/src/components/CategoryPage/index.js
+++ react-app/src/components/CategoryPage/index.js
@@ -32,8 +32,8 @@ const CategoryPage = () => {
                       : "https://media.istockphoto.com/vectors/no-image-available-sign-vector-id922962354?k=20&m=922962354&s=612x612&w=0&h=f-9tPXlFXtz9vg_-WonCXKCdBuPUevOBkp3DQ-i0xqo="
                   } />
               </div>
-              <div className="product-name">{product?.name}</div>
-              <div className="product-price">${product?.price}</div>
+              <div className="product_name">{product?.name}</div>
+              <div className="product_price">${parseFloat(product?.price).toFixed(2)}</div>
             </div>
           </Link>
         ))}
