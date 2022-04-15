c5b42bec0c068a13aa35cbdeffe3a017827ff3e3
Laura <laura.maser3@yahoo.com>
2022-04-15 11:02:30 -0400

    Minor changes to remove console errors and unused vars.

--- react-app/src/components/SearchBar.js
+++ react-app/src/components/SearchBar.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { useHistory, Link } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { findResults } from '../store/results';
@@ -6,33 +6,10 @@ import { findResults } from '../store/results';
 const SearchBar = () => {
     const products = useSelector(state => state.productsReducer)
     const [searchTerm, setSearchTerm] = useState('');
-    const [listProducts, setListProducts] = useState([])
     const history = useHistory();
     const dispatch = useDispatch();
 
     let allProducts = Object.values(products)
-    // useEffect(() => {
-    //     if (searchTerm.length > 0) {
-    //         filterProducts(searchTerm)
-    //     }
-    // }, [searchTerm])
-
-    // const filterProducts = (searchTerm) => {
-    //     let allProducts = Object.values(products)
-
-    //     allProducts.forEach((product) => {
-    //         console.log("SearchTerm!!!!!!!!!!", searchTerm)
-    //         if (product?.name.toLowerCase().includes(searchTerm.toLowerCase())) {
-
-    //             console.log("PRODUCT", product.name)
-    //             setListProducts(product.name)
-    //             console.log("HERE!!!!!!!!!", listProducts)
-
-    //             // setListProducts(addToList)
-    //             // console.log("##############", listProducts)
-    //         }
-    //     })
-    // }
 
     const handleSubmit = (e) => {
         e.preventDefault()
@@ -63,20 +40,24 @@ const SearchBar = () => {
                 </button>
             </form>
             <div hidden={!searchTerm} className='search-bar-results-div'>
-                {allProducts.map((product) => {
-                    if (product?.name.toLowerCase().includes(searchTerm.toLowerCase())) {
-                        count++
-                        return (
-                            <li>
-                                <Link to={`/products/${product.id}`}
-                                    onClick={() => setSearchTerm('')}
-                                >
-                                    {product.name}
-                                </Link>
-                            </li>)
-                    }
-                })}
-                {!count ? <li> No results found </li> : null}
+                <ul>
+                    {allProducts.map((product) => {
+                        if (product?.name.toLowerCase().includes(searchTerm.toLowerCase())) {
+                            count++
+                            return (
+                                <li key={product.id}>
+                                    <Link to={`/products/${product.id}`}
+                                        onClick={() => setSearchTerm('')}
+                                    >
+                                        {product.name}
+                                    </Link>
+                                </li>)
+                        } else {
+                            return null;
+                        }
+                    })}
+                    {!count ? <li> No results found </li> : null}
+                </ul>
             </div>
         </>
     )
